fc00762448ca74ceedd611a8124b40027c7f373e
nabacg <nabacg@gmail.com>
2014-11-16 13:58:17 +0000

    issue #5 temp add austin to heroku build

--- project.clj
+++ project.clj
@@ -24,7 +24,8 @@
   :source-paths ["src/clj" "src/cljs"]
   :plugins [[lein-cljsbuild "1.0.4-SNAPSHOT"]
             [lein-pdo "0.1.1"]
-            [jarohen/lein-frodo "0.4.1"]]
+            [jarohen/lein-frodo "0.4.1"]
+            [com.cemerick/austin "0.1.5"]]
   :aliases {"up" ["pdo" "cljsbuild" "auto" "dev," "frodo"]
             "prod-release" ["pdo" "cljsbuild" "once" "release," "frodo"]}
   :frodo/config-resource "config.edn"
